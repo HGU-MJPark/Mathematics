bebc5388c809d69e6efd16d50600652ddd64e3cb
handongdata <70467382+handongdata@users.noreply.github.com>
2020-09-27 04:42:51 +0900

    Update sudokuX.c

--- sudokuX.c
+++ sudokuX.c
@@ -87,11 +87,10 @@ int main(void){
   fprintf(fp,"))\n");
 
   fprintf(fp,"(assert ");
-  //fprintf(fp,"(or ");
   fprintf(fp,"(and ");
   for (i=1 ; i<=9 ; i++){
       j=i;
-
+      fprintf(fp,"(or ");
       for (n=1 ; n<=9 ; n++){
           if (p[i-1][j-1]==0)
               fprintf(fp,"p%d%d%d ",i,j,n);
@@ -103,11 +102,10 @@ int main(void){
   fprintf(fp,"))\n");
 
   fprintf(fp,"(assert ");
-  //fprintf(fp,"(or ");
   fprintf(fp,"(and ");
   for (i=1 ; i<=9 ; i++){
       j=10-i;
-
+      fprintf(fp,"(or ");
       for (n=1 ; n<=9 ; n++){
           if (p[i-1][j-1]==0)
               fprintf(fp,"p%d%d%d ",i,j,n);
