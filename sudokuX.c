f73764bc80ca0c94d5f9ce6dbdcaa1b7ec6530ea
handongdata <70467382+handongdata@users.noreply.github.com>
2020-09-27 22:56:44 +0900

    Update sudokuX.c

--- sudokuX.c
+++ sudokuX.c
@@ -20,8 +20,8 @@ int main(void){
 
   for (i=1; i<=9; i++)
       for (j=1; j<=9; j++)
-          //for (n=1; n<=9; n++)
-              fprintf(fp,"(declare-const p%d%d%d Bool)\n", i,j,p[i-1][j-1]);
+          for (n=1; n<=9; n++)
+              fprintf(fp,"(declare-const p%d%d%d Bool)\n", i,j,n);
 
 
   fprintf(fp,"(assert (and ");
