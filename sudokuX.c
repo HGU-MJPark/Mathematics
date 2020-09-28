ae6acd3439800be08a941cfd5b6d9065b0f5ba07
handongdata <70467382+handongdata@users.noreply.github.com>
2020-09-29 04:16:39 +0900

    Update sudokuX.c

--- sudokuX.c
+++ sudokuX.c
@@ -1,156 +1,128 @@
 #include <stdio.h>
 
 int main(void){
+    FILE * fp = fopen("formula", "w") ;
 
-  FILE * fp = fopen("formula", "w");
+    int i, j, r, s, n, m;
 
-  int i,j,n,r,s,k,l;
-
-  int p[9][9]={
-    {0,3,0,8,4,0,0,0,0},
-    {0,0,0,9,0,0,0,0,0},
-    {0,0,5,0,0,0,0,0,0},
-    {2,5,0,0,0,7,4,8,0},
-    {0,0,1,0,0,0,0,3,0},
-    {0,7,3,0,0,0,0,0,1},
-    {0,4,0,0,0,0,0,0,0},
-    {0,0,8,6,0,0,9,0,0},
-    {9,0,0,0,0,0,0,0,0}
-  };
-
-  for (i=1; i<=9; i++)
-      for (j=1; j<=9; j++)
-          for (n=1; n<=9; n++)
-          //if(p[i-1][j-1]==n){
-              fprintf(fp,"(declare-const p%d%d%d Bool)\n", i,j,n);
-              //break;
-          //}
-          //else
-              //fprintf(fp,"(declare-const p%d%d%d Bool)\n", i,j,n);
-      //}
-     // }
-  //}
-
-
-  fprintf(fp,"(assert (and ");
-  for (i=1 ; i<=9 ; i++){
-      fprintf(fp,"(and ");
-      for (n=1 ; n<=9 ; n++){
-          fprintf(fp,"(or ");
-          for(k=1; k<=9; k++){
-              if(p[i-1][k-1]==n){
-                  fprintf(fp,"p%d%d%d ",i,k,n);
-                  break;
-              }
-          }
-          if(k==10){
-              for(j=1; j<=9; j++)
-                  fprintf(fp,"p%d%d%d ", i,j,n);
-                  }
-          fprintf(fp,")");
-      }
-      fprintf(fp,")");
-  }
-  fprintf(fp,"))\n");
-
-  fprintf(fp,"(assert (and ");
-  for (j=1 ; j<=9 ; j++){
-      fprintf(fp,"(and ");
-      for (n=1 ; n<=9 ; n++){
-          fprintf(fp,"(or ");
-          for(k=1; k<=9; k++){
-              if(p[k-1][j-1]==n){
-                  fprintf(fp,"p%d%d%d ",k,j,n);
-                  break;
-              }
-          }
-          if(k==10){
-              for(i=1; i<=9; i++)
-                  fprintf(fp,"p%d%d%d ", i,j,n);
-                  }
-              fprintf(fp,")");
-      }
-      fprintf(fp,")");
-  }
-  fprintf(fp,"))\n");
-
-  fprintf(fp,"(assert ");
-  fprintf(fp,"(and ");
-  for (r=0 ; r<=2 ; r++){
-      fprintf(fp,"(and ");
-      for (s=0 ; s<=2 ; s++){
-          fprintf(fp,"(and ");
-          for (n=1 ; n<=9 ; n++){
-              fprintf(fp,"(or ");
-              for(l=1; l<=3; l++){
-                  for(k=1; k<=3; k++){
-                      if(p[3*r+l-1][3*s+k-1]==n){
-                          fprintf(fp,"(or p%d%d%d)",3*r+l,3*s+k,n);
-                          l=10;
-                      }
-                  }
-              }
-              if(l!=11){
-                  for(i=1; i<=3; i++){
-                      fprintf(fp,"(or ");
-                      for(j=1; j<=3; j++)
-                          fprintf(fp,"p%d%d%d ", 3*r+i, 3*s+j, n);
-                      fprintf(fp,")");
-                  }
-              }
-              fprintf(fp,")");
-          }
-          fprintf(fp,")");
-      }
-      fprintf(fp,")");
-  }
-  fprintf(fp,"))\n");
-
-
-  fprintf(fp,"(assert ");
-  fprintf(fp,"(and ");
-  for(n=1; n<=9; n++){
-      fprintf(fp,"(or ");
-    for(k=1; k<=9; k++){
-        if(p[k-1][k-1]==n){
-            fprintf(fp,"p%d%d%d ",k,k,n);
-            break;
+    for (i = 1 ; i <= 9 ; i++)
+        for (j = 1 ; j <= 9 ; j++)
+            for(n=1; n<=9; n++)
+                fprintf(fp,"(declare-const p%d%d%d Bool)\n", i, j,n) ;
+    
+    fprintf(fp, "(assert (and p123 p148 p154 p249 p335 p412 p425 p467 p474 p488 p531 p583 p627 p633 p691 p724 p838 p846 p879 p919))");
+    
+    fprintf(fp,"(assert (and ");
+    for(i=1; i<=9; i++){
+        fprintf(fp,"(and ");
+        for(j=1; j<=9; j++){
+            fprintf(fp,"(and ");
+            for(n=1; n<=8; n++){
+                fprintf(fp,"(and ");
+                for(m=n+1; m<=9; m++){
+                    fprintf(fp,"(not (and p%d%d%d p%d%d%d))", i, j, n, i, j,m) ;
+                }
+                fprintf(fp,")");
+            }
+            fprintf(fp,")");
         }
+        fprintf(fp,")");
     }
-      if(k==10)
-          for(i=1;i<=9;i++)
-              fprintf(fp,"p%d%d%d ",i,i,n);
-      fprintf(fp,")");
-  }
-    fprintf(fp, "))\n");
-  
-
-
-  fprintf(fp,"(assert ");
-  fprintf(fp,"(and ");
-  for(n=1; n<=9; n++){
-      fprintf(fp,"(or ");
-      for(k=1; k<=9; k++){
-        if(p[k-1][9-k]==n){
-            fprintf(fp,"p%d%d%d ",k,10-k,n);
-            break;
-        }
-    }
-      if(k==10)
-          for(i=1;i<=9;i++)
-              fprintf(fp,"p%d%d%d ",i,10-i,n);
-          fprintf(fp,")");
-      }
     fprintf(fp,"))\n");
-  
-  
+    
+    fprintf(fp,"(assert (and ");
+    for(i=1; i<=9; i++){
+        fprintf(fp,"(and ");
+        for(j=1; j<=9; j++){
+            fprintf(fp,"(or ");
+            for(n=1; n<=9; n++){
+                fprintf(fp,"p%d%d%d ", i, j, n);
+            }
+            fprintf(fp,")");
+        }
+        fprintf(fp,")");
+    }
+    fprintf(fp,")");
+    
+    
+    fprintf(fp,"(assert (and ");
+    for (i=1 ; i<=9 ; i++){
+        fprintf(fp,"(and ");
+        for (n=1 ; n<=9 ; n++){
+            fprintf(fp,"(or ");
+            for(j=1; j<=9; j++){
+                fprintf(fp,"p%d%d%d ", i,j,n);
+            }
+            fprintf(fp,")");
+        }
+        fprintf(fp,")");
+    }
+    fprintf(fp,"))\n");
+    
+    fprintf(fp,"(assert (and ");
+    for (j=1 ; j<=9 ; j++){
+        fprintf(fp,"(and ");
+        for (n=1 ; n<=9 ; n++){
+            fprintf(fp,"(or ");
+            for(i=1; i<=9; i++){
+                fprintf(fp,"p%d%d%d ", i,j,n);
+            }
+            fprintf(fp,")");
+        }
+        fprintf(fp,")");
+    }
+    fprintf(fp,"))\n");
+    
+    fprintf(fp,"(assert ");
+    fprintf(fp,"(and ");
+    for (r=0 ; r<=2 ; r++){
+        fprintf(fp,"(and ");
+        for (s=0 ; s<=2 ; s++){
+            fprintf(fp,"(and ");
+            for (n=1 ; n<=9 ; n++){
+                fprintf(fp,"(or ");
+                for(i=1; i<=3; i++){
+                    fprintf(fp,"(or ");
+                    for(j=1; j<=3; j++){
+                        fprintf(fp,"p%d%d%d ",3*r+i,3*s+j,n);
+                    }
+                    fprintf(fp,")");
+                }
+                fprintf(fp,")");
+            }
+            fprintf(fp,")");
+        }
+        fprintf(fp,")");
+    }
+    fprintf(fp,"))\n");
+    
+    fprintf(fp,"(assert ");
+    fprintf(fp,"(and ");
+    for(n=1; n<=9; n++){
+        fprintf(fp,"(or ");
+        for(i=1;i<=9;i++){
+                fprintf(fp,"p%d%d%d ",i,i,n);
+        }
+        fprintf(fp,")");
+    }
+    fprintf(fp, "))\n");
+    
+    fprintf(fp,"(assert ");
+    fprintf(fp,"(and ");
+    for(n=1; n<=9; n++){
+        fprintf(fp,"(or ");
+        for(i=1;i<=9;i++){
+                fprintf(fp,"p%d%d%d ",i,10-i,n);
+        }
+        fprintf(fp,")");
+    }
+    fprintf(fp,"))\n");
+    
+    fprintf(fp,"(check-sat)\n(get-model)\n") ;
 
+    fclose(fp) ;
 
-  fprintf(fp,"(check-sat)\n(get-model)\n") ;
-
-  fclose(fp);
-
-  FILE * fin = popen("z3 formula", "r") ;
+    FILE * fin = popen("z3 formula", "r") ;
     char buf[128] ;
     fscanf(fin, "%s %s", buf, buf) ;
     while (!feof(fin)) {
@@ -160,8 +132,7 @@ int main(void){
         fscanf(fin, "%s", buf) ; printf("%s ", buf) ;
         fscanf(fin, "%s", buf) ; printf("%s\n", buf) ;
     }
-    pclose(fin) ;
-
+    pclose(fin);
 
 }
 
