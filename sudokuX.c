59484b90af0507387f6d946ca5499ba458aa9eee
handongdata <70467382+handongdata@users.noreply.github.com>
2020-09-29 19:08:44 +0900

    Update sudokuX.c

--- sudokuX.c
+++ sudokuX.c
@@ -13,8 +13,22 @@ int main(void){
         }
     }
     
-    fprintf(fp, "(assert (and p123 p148 p154 p249 p335 p412 p425 p467 p474 p488 p531 p583 p627 p633 p691 p724 p838 p846 p879 p919))");
+    fprintf(fp, "(assert (and ");
+    printf("row () column () 의 값을 입력하시오. 비어있는 값은 0을 입력하시오. ");
+    for(i=1; i<=9; i++){
+        for(j=1; j<=9; j++){
+            printf("row %d column %d 의 값을 입력하시오. 비어있는 값은 0을 입력하시오. ",i,j);
+            scanf("%d", &n);
+            if(n!=0)
+                fprintf(fp,"p%d%d%d ",i,j,n);
+        }
+    }
+    fprintf(fp, "))\n");
     
+    /*
+    fprintf(fp, "(assert (and p123 p148 p154 p249 p335 p412 p425 p467 p474 p488 p531 p583 p627 p633 p691 p724 p838 p846 p879 p919))");
+    */
+     
     fprintf(fp,"(assert (and ");
     for(i=1; i<=9; i++){
         fprintf(fp,"(and ");
