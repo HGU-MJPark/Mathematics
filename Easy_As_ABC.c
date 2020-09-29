344b678d83d59627c6c65aca0b5c93475b6740dc
handongdata <70467382+handongdata@users.noreply.github.com>
2020-09-29 12:58:10 +0900

    Update Easy_As_ABC.c

--- Easy_As_ABC.c
+++ Easy_As_ABC.c
@@ -6,13 +6,7 @@ int main(void){
     int i,j,n,m;
     char s[6]={' ','A','B','C','D','E'};
     
-    for(i=1; i<=6; i++){
-        for(j=1; j<=6; j++){
-            for(n=0; n<6; n++){
-                fprintf(fp, "(declare-const p%d%d%c Bool)\n", i, j, s[n]);
-            }
-        }
-    }
+    
     
     fprintf(fp, "(assert (and (or p11s[1] p21s[1])(or p14s[5] p24s[5])(or p31s[1] p32s[1])(or p41s[4] p42s[4])(or p62s[2] p52s[2])(or p63s[3] p53s[3])(or p66s[1] p56s[1])(or p16s[5] p15s[5])(or p36s[2] p35s[2])(or p46s[5] p45s[5])))");
     
@@ -21,9 +15,9 @@ int main(void){
         fprintf(fp,"(and ");
         for(j=1; j<=6; j++){
             fprintf(fp,"(and ");
-            for(n=0; n<=4; n++){
+            for(n=0; n<5; n++){
                 fprintf(fp,"(and ");
-                for(m=n+1; m<=5; m++){
+                for(m=n+1; m<6; m++){
                     fprintf(fp,"(not (and p%d%d%c p%d%d%c))", i, j, s[n], i, j,s[m]) ;
                 }
                 fprintf(fp,")");
@@ -33,8 +27,6 @@ int main(void){
         fprintf(fp,")");
     }
     fprintf(fp,"))\n");
-    
-    
     
     fprintf(fp,"(assert (and ");
     for(i=1; i<=6; i++){
