b9b514348df12efe6d132cb164a61abbdb31bd2d
handongdata <70467382+handongdata@users.noreply.github.com>
2020-09-30 10:31:59 +0900

    Update sudokuX.c

--- sudokuX.c
+++ sudokuX.c
@@ -149,7 +149,7 @@ int main(void){
         fscanf(fin, "%s", buf) ; printf("%s ", buf) ;
         fscanf(fin, "%s", buf) ; printf("%s ", buf) ;
                 strncpy(output, buf,4);
-        printf("%c",output[1]);
+        printf("%c ",output[1]);
         printf("%c",output[2]);
         fscanf(fin, "%s", buf) ; printf("%s ", buf) ;
         fscanf(fin, "%s", buf) ; printf("%s ", buf) ;
